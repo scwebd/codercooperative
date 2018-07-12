ec1dead8cce3edb6901adcc14f4b37e6cdcba30d
scwebd <scwebd@gmail.com>
2018-07-11 18:35:06 -0600

    Mucking w/server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(
 app.use(bodyParser.json())
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("../client/build"));
+	app.use(express.static("/client/build"));
 }
 
 // Sessions
